12f732af789e6274d0778347c2eb1ef3b6a090aa
kcdissanayaka <kc.dissanayaka@outlook.com>
2024-02-15 09:24:41 +0200

    createBKPdbflie

---
+++ bkpDBfiles/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "phpsrv-db-1"; /*"phpsrv-db-1"; Kasun DB */
+$username = "lankanmtka";
+$password = "1234";
+$dbname = "lankanmtka";
+
+// create db connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// check db connection
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
