ef656867f9a49f1727a55f65e10f2077076d7d35
kcdissanayaka <kc.dissanayaka@outlook.com>
2024-02-19 12:39:01 +0200

    updateMarginSeetings

--- travelPkgs.php
+++ travelPkgs.php
@@ -33,8 +33,7 @@ if ($result) {
                 </div>
                 <div class="container text-center mt-2">
                     <!--<div class="row tourPlanCards g-3"> -->
-                    <!--<div class="row m-0 row row-cols-1 row-cols-md-3 g-4">--> 
-                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="row m-0 tourPlanCards">
                          <?php 
                             foreach ($tourPlanList as $tourPlan) {
                                //var_dump($tourPlan);
